bfbe55c22a361057cc2eee69f4c55d0ad8b653d9
R09725064 <r09725064@ntu.edu.tw>
2021-04-05 00:28:29 +0800

    refactor(app): adjust indent spaces

--- app/src/.eslintrc.js
+++ app/src/.eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
         'linebreak-style': 0,
         'prefer-template': 0,
         'arrow-parens': ['error', 'always'],
-        indent: ['off', 2],
+        indent: [2, 4],
         'newline-per-chained-call': 0,
         'operator-linebreak': ['error', 'after'],
         'no-use-before-define': [
--- app/src/pages/search/index.js
+++ app/src/pages/search/index.js
@@ -4,15 +4,15 @@ import Searchform from '../../components/search/form';
 import '../../assets/style/page/search/index.scss';
 
 function output() {
-  return (
-    <div>
-      <Searchform />
-      <Informationcard />
-      <br></br>
-      <Informationcard />
-      <br></br>
-      <Informationcard />
-    </div>
-  );
+    return (
+        <div>
+            <Searchform />
+            <Informationcard />
+            <br></br>
+            <Informationcard />
+            <br></br>
+            <Informationcard />
+        </div>
+    );
 }
 export default output;
