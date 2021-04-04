d48d30a88c07e99fe77e612f1d5e5a42bfce5373
arita1611 <arita1611@yahoo.com.tw>
2021-04-04 15:37:16 +0000

    Revert "Merge branch 'feature/create-pet-information' into 'master'"
    This reverts merge request !16

--- app/src/pages/search/index.js
+++ app/src/pages/search/index.js
@@ -1,18 +1,3 @@
-import React from 'react';
-import Informationcard from '../../components/search/index';
-import Searchform from '../../components/search/form';
-import '../../assets/style/page/search/index.scss';
+const Search = () => <div className="search">This is Search Page.</div>;
 
-function output() {
-  return (
-    <div>
-      <Searchform />
-      <Informationcard />
-      <br></br>
-      <Informationcard />
-      <br></br>
-      <Informationcard />
-    </div>
-  );
-}
-export default output;
+export default Search;
