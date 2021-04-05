fba63fea1fc42bff90b728bab3dcc91179625730
Yutse Wu <allen010301@gmail.com>
2021-04-05 09:32:02 +0000

    feat(app): seperate login form to component

--- app/src/pages/login/index.js
+++ app/src/pages/login/index.js
@@ -1,42 +1,13 @@
-import { Button, Input } from 'antd';
+import '../../assets/style/login/index.scss';
+import LoginForm from '../../components/login/LoginForm';
 
 const Login = () => (
     <div className="login-page">
         <div className="left-container">
-            <div className="login-form">
-                <header className="login-form-title">會員登入</header>
-                <div className="account">
-                    <p>
-                        <label className="account-label">帳號</label>
-                        <Input type="text" className="accout-text" id="accout-text" placeholder="Username" />
-                    </p>
-                </div>
-                <div className="password">
-                    <p>
-                        <label className="password-label">密碼</label>
-                        <Input type="password" className="password-text" id="password-text" placeholder="Password" />
-                    </p>
-                </div>
-                <Button className="login-button">登入</Button>
-            </div>
+            <LoginForm type='login' />
         </div>
         <div className="right-container">
-            <div className="register-form">
-                <header className="register-form-title">會員註冊</header>
-                <div className="account">
-                    <p>
-                        <label className="account-label">帳號</label>
-                        <Input type="text" className="accout-text" id="accout-text" placeholder="Username" />
-                    </p>
-                </div>
-                <div className="password">
-                    <p>
-                        <label className="password-label">密碼</label>
-                        <Input type="password" className="password-text" id="password-text" placeholder="Password" />
-                    </p>
-                </div>
-                <Button className="register-button">註冊</Button>
-            </div>
+            <LoginForm type='register' />
         </div>
     </div>
 );
