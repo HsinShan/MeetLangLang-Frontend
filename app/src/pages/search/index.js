7c9fbb65e9f27740fdcb4c23d582426f1d4bdafa
R09725064 <r09725064@ntu.edu.tw>
2021-04-05 22:21:23 +0800

    refactor(app):  name is exist so change

--- app/src/pages/search/index.js
+++ app/src/pages/search/index.js
@@ -3,7 +3,7 @@ import Informationcard from '../../components/search/InformationCard';
 import Searchform from '../../components/search/form';
 import '../../assets/style/search/index.scss';
 
-function elements() {
+function cardelements() {
     const elements = [];
     let i = 0;
     for (i = 0; i < 5; i += 1) {
@@ -17,9 +17,9 @@ function search() {
         <div>
             <Searchform />
             <br></br>
-            <div className="row">{elements()}</div>
-            <div className="row">{elements()}</div>
-            <div className="row">{elements()}</div>
+            <div className="row">{cardelements()}</div>
+            <div className="row">{cardelements()}</div>
+            <div className="row">{cardelements()}</div>
         </div>
     );
 }
