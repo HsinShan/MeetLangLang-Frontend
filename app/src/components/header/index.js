c4bd6920f035f27b19e1e0ab12fb757b074a2495
HsinShan <shirleyhsieh825@gmail.com>
2021-04-09 11:16:09 +0800

    feat(app): add logo to header

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -1,10 +1,14 @@
 import Navbar from './navbar';
 import UserHeader from './userHeader';
+import Logo from '../../assets/images/logo.svg';
 
 const Header = ({ isLogin, logout }) => (
     <div className="header">
         <div>
-            <div className="logo">Meet Lang Lang</div>
+            <div className="logo">
+                <h1>遇見浪浪</h1>
+                <img src={Logo} alt="Logo" />
+            </div>
         </div>
         <div className="container">
             <Navbar />
