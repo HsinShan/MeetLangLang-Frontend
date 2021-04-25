55e5df85e756842d1b34c440fd6c270c243d5436
GengBao.Lin <lernhern@gmail.com>
2021-04-26 04:45:06 +0800

    style(app): adjust style for discuss adding page

--- app/src/pages/adddiscuss/index.js
+++ app/src/pages/adddiscuss/index.js
@@ -9,6 +9,7 @@ import {
     message,
 } from 'antd';
 import axios from 'axios';
+import '../../assets/style/adddiscuss/index.scss';
 
 const apiProtocol = process.env.REACT_APP_API_PROTOCOL;
 const apiPort = process.env.REACT_APP_API_PORT;
@@ -42,54 +43,54 @@ const AddDiscuss = ({ isLogin }) => {
         }
     };
     return (
-        <>
+        <div className="adddiscuss">
             {!isLogin &&
                 <Redirect to="/" />
             }
             {redirect !== '' &&
                 <Redirect to={redirect} />
             }
-            <Row justify="center">
-                <Col span={4}>
+            <Row className="row-input" justify="center">
+                <Col className="inputtitle" span={3}>
                     作者
                 </Col>
-                <Col span={16}>
+                <Col span={15}>
                     製作中...
                 </Col>
             </Row>
-            <Row justify="center">
-                <Col span={4}>
+            <Row className="row-input" justify="center">
+                <Col className="inputtitle" span={3}>
                     主題
                 </Col>
-                <Col span={16}>
+                <Col span={15}>
                     <Input placeholder="大家都怎麼照顧幼犬的呢？" onChange={(e) => setTitle(e.target.value)} />
                 </Col>
             </Row>
-            <Row justify="center">
-                <Col span={4}>
+            <Row className="row-input" justify="center">
+                <Col className="inputtitle" span={3}>
                     類別
                 </Col>
-                <Col span={16}>
+                <Col span={15}>
                     <Select defaultValue="default" disabled>
                         <Select.Option value="default">製作中...</Select.Option>
                     </Select>
                 </Col>
             </Row>
-            <Row justify="center">
-                <Col span={4}>
+            <Row className="row-input" justify="center">
+                <Col className="inputtitle" span={3}>
                     內容
                 </Col>
-                <Col span={16}>
+                <Col span={15}>
                     <Input.TextArea rows={4} placeholder="家裡誕生了新成員，想問大家都怎麼照顧幼犬的呢？" onChange={(e) => setContent(e.target.value)} />
                 </Col>
             </Row>
-            <Row justify="end">
-                <Col span={20}>
+            <Row className="row-input bottom" justify="end">
+                <Col className="col-buttons" span={18} pull={3}>
                     <Button type="primary" onClick={() => submit()}>張貼</Button>
                     <Button type="primary"><Link to="/">取消</Link></Button>
                 </Col>
             </Row>
-        </>
+        </div>
     );
 };
 
