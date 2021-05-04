953a7152ca8b5fb21294a00a4ba33752d0e40b04
R09725064 <r09725064@ntu.edu.tw>
2021-05-04 16:57:33 +0800

    feat(app): add field title and i18n

--- app/src/pages/discuss/detail.js
+++ app/src/pages/discuss/detail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
-import { Card } from 'antd';
+import { Card, Spin } from 'antd';
+import { useTranslation } from 'react-i18next';
 import '../../assets/style/discuss/detail.scss';
 
 // use api
@@ -9,42 +9,35 @@ const apiProtocol = process.env.REACT_APP_API_PROTOCOL;
 const apiPort = process.env.REACT_APP_API_PORT;
 
 function DiscussDetail() {
-    const location = useLocation();
-    const [title, setTitle] = useState(null);
-    const [content, setContent] = useState(null);
-    // 有無帶參數
-    const searchURL = location.search;
-    const bool = searchURL.indexOf('?');
-    // 判斷uuid有沒有帶值
-    if (!('search' in location) || !location.search) window.location.href = '/discuss';
-    if (bool === '-1' || !location.search) window.location.href = '/discuss';
-    // 取得uuid
+    const { t } = useTranslation();
     const { search } = window.location;
     const params = new URLSearchParams(search);
     const uuidFromURL = params.get('uuid');
+    const [discuss, setDiscuss] = useState(null);
     const getData = async () => {
         const { hostname } = window.location;
         await axios.get(`${apiProtocol}://${hostname}:${apiPort}/discuss/topic/detail/${uuidFromURL}`)
             .then((res) => {
                 const { data } = res;
-                setTitle(data[0].title);
-                setContent(data[0].content);
+                setDiscuss(data[0]);
             });
     };
     useEffect(() => {
-        if (uuidFromURL && title === null && content === null) {
+        if (uuidFromURL && discuss === null) {
             getData();
         }
     });
 
     return (
         <>
-            { title !== null && content !== null &&
-                <Card title={title}>
-                    <p>{content}</p>
+            { discuss !== null &&
+                <Card title={discuss.title}>
+                    <p className="author">{t('discuss.author')}：{discuss.author}</p>
+                    <p className="date">{t('discuss.date')}：{discuss.date}</p>
+                    <p className="contnet">{t('discuss.content')}：{discuss.content}</p>
                 </Card>
             }
-            { title === null && content === null && <p>error</p> }
+            { discuss === null && uuidFromURL === null && <div><Spin tip="Loading..." /></div>}
         </>
     );
 }
