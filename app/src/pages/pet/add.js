292caf15d58f4eca3954d5ed55b52a0251348232
R09725064 <r09725064@ntu.edu.tw>
2021-05-10 20:09:54 +0800

    feat(app): imgur upload image

--- app/src/pages/pet/add.js
+++ app/src/pages/pet/add.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import PetForm from '../../components/pet/petForm';
 
 const AddPet = () => {
     const { t } = useTranslation();
     return (
-        <div className="draw-cards">{t('drawcards.building')}</div>
+        <div>{t('petadd.pet-name')}</div>,
+        <div className="petform">
+            <PetForm/>
+        </div>
     );
 };
 export default AddPet;
