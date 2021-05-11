cbdfae4e7d3cdac44997b879b637eb1e0b6bec23
R09725064 <r09725064@ntu.edu.tw>
2021-05-12 00:11:52 +0800

    feat(app): can use imgur post and get image

--- app/src/components/pet/petForm.js
+++ app/src/components/pet/petForm.js
@@ -1,17 +1,16 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
-// import { VerticalAlignTopOutlined } from '@ant-design/icons';
 import {
-    Form, Input, Upload,
+    Form, Input, Upload, Button, Radio,
 } from 'antd';
 import { PlusOutlined, LoadingOutlined } from '@ant-design/icons';
-// import { UploadOutlined } from '@ant-design/icons';
 import '../../assets/style/pet/petForm.scss';
-import { useState } from 'react';
 
 const PetForm = () => {
     const { t } = useTranslation();
-    // const [selectFile, setSelectFile] = setState(null);
+    const { TextArea } = Input;
+    const [information, setInformation] = useState(false);
     const [loading, setLoading] = useState(false);
     const [imageUrl, setImageUrl] = useState(null);
     const uploadButton = (
@@ -20,94 +19,61 @@ const PetForm = () => {
             <div style={{ marginTop: 8 }}>Upload</div>
         </>
     );
-    function getBase64(img, callback) {
-        console.log('start');
-        const reader = new FileReader();
-        reader.addEventListener('load', () => callback(reader.result));
-        console.log(reader.readAsDataURL(img));
-        return reader.readAsDataURL(img);
-    }
-    const handleChange = (info) => {
-        console.log('info', info);
-        // if (info.file.thumbUrl) {
-        //     setLoading(true);
-        //     setImageUrl(info.file.thumbUrl);
-        //     setLoading(false);
-        // }
-        if (info.file.status === 'uploading') {
-            return setLoading(true);
-        }
-        if (info.file.status === 'done') {
-            // Get this url from response in real world.
-            // console.log(getBase64(info.file.originFileObj));
-            // const url = getBase64(info.file.originFileObj);
-            // setImageUrl(url);
-            getBase64(info.file.originFileObj, (imgUrl) => {
-                console.log(imgUrl);
-                setImageUrl(imgUrl);
-                setLoading(false);
-            });
-        }
-        return null;
-    };
     const customRequest = async (e) => {
-        console.log(e);
-        const { file } = e;
-        axios.post('https://api.imgur.com/3/image', {
-            image: file.name,
-        },
-        {
+        setLoading(true);
+        const imgurForm = new FormData();
+        imgurForm.append('image', e.file);
+        const config = {
             headers: {
                 Authorization: 'Client-ID 7250f9c19e46352',
-                // 'x-post-rate-limit-limit': [1250],
-                // 'x-post-rate-limit-remaining': [1250],
-                // 'x-post-rate-limit-reset': [-2],
-                // 'x-ratelimit-clientlimit': [12500],
-                // 'x-ratelimit-clientremaining': [12499],
-                // 'x-ratelimit-userlimit': [500],
-                // 'x-ratelimit-userremaining': [499],
-                // 'x-ratelimit-userreset': [1620619870],
             },
-        });
+        };
+        axios.post('https://api.imgur.com/3/image', imgurForm, config)
+            .then((res) => {
+                setImageUrl(res.data.data.link);
+                setLoading(false);
+            });
     };
-    /*
-    const fileSelectHandler = (event) => {
-        setSelectFile(event.target.files[0]);
-        console.log('fileSelect');
-        console.log(event);
-        console.log(event.target.files[0]);
+    const onChange = (e) => {
+        const { value } = e.target;
+        if (value.length !== 0) {
+            setInformation(true);
+        }
+        if (value.length === 0) {
+            setInformation(null);
+        }
     };
-    const fileUploadHandler = () => {
-
+    const onFinish = (allvalues) => {
+        console.log('Received values of form: ', allvalues);
     };
-    */
     return (
         <div className="pet-form">
-            <Form>
-                <Form.Item className="pet-input" label={t('petadd.pet-name')}>
+            <Form onFinish={onFinish}>
+                <Form.Item className="pet-input" label={t('petadd.pet-name')} name="name" rules={[{ required: true, message: 'Please input your petname!' }]}>
+                    <Input onChange={onChange}/>
+                </Form.Item>
+                <Form.Item className="pet-input" label={t('petadd.pet-sex')} name="sex">
+                    <Radio.Group>
+                        <Radio value="F">{t('petadd.male')}</Radio>
+                        <Radio value="M">{t('petadd.female')}</Radio>
+                        <Radio value="null">{t('petadd.null')}</Radio>
+                    </Radio.Group>
+                </Form.Item>
+                <Form.Item className="pet-input" label={t('petadd.pet-age')} name="age">
                     <Input />
                 </Form.Item>
-                <Form.Item className="pet-input" label={t('petadd.pet-sex')}>
+                <Form.Item className="pet-input" label={t('petadd.pet-kind')} name="kind">
                     <Input />
                 </Form.Item>
-                <Form.Item className="pet-input" label={t('petadd.pet-age')}>
-                    <Input />
+                <Form.Item className="pet-input" label={t('petadd.pet-introduction')} name="introduction">
+                    <TextArea autoSize={{ minRows: 3, maxRows: 5 }} />
                 </Form.Item>
-                <Form.Item className="pet-input" label={t('petadd.pet-kind')}>
-                    <Input />
-                </Form.Item>
-                <Form.Item className="pet-input" label={t('petadd.pet-introduction')}>
-                    <Input />
+                <Form.Item>
+                    {information ? <Button type="Default" htmlType="submit">{t('petadd.add')}</Button> : <Button type="Default" disabled>{t('petadd.add')}</Button>}
                 </Form.Item>
             </Form>
-            {/* <Upload>
-                <Button icon={<UploadOutlined />}>Click to Upload</Button>
-            </Upload> */}
-            {/* <input type="file" onChange={fileSelectHandler} />
-            <Button onclick={() => fileUploadHandler} shape="circle" icon={<VerticalAlignTopOutlined />}/> */}
-            {/* <button onclick={() => fileUploadHandler} shape="circle" icon={<VerticalAlignTopOutlined />}></button> */}
-            <Upload className="avatar-uploader pet-image" listType="picture-card" action="https://api.imgur.com/3/image" customRequest={customRequest} onChange={handleChange}>
-                {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+            <Upload className="avatar-uploader pet-image" listType="picture-card" action="https://api.imgur.com/3/image" showUploadList={false} customRequest={customRequest}>
+                {imageUrl ? <img src={`https://images.weserv.nl/?url=${imageUrl}`} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
             </Upload>
         </div>
     );
