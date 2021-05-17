d52d1ebb4bf1bcff89e74dacd0068456f9d97bcb
R09725064 <r09725064@ntu.edu.tw>
2021-05-17 16:58:23 +0800

    feat(app): show test data

--- app/src/components/drawcards/drawCardPage.js
+++ app/src/components/drawcards/drawCardPage.js
@@ -13,10 +13,10 @@ const apiPort = process.env.REACT_APP_API_PORT;
 
 const DrawCardsPage = () => {
     const { t } = useTranslation();
-    const [pets, setPets] = useState(null);
+    const [havePets, setHavePets] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [drawcard, setDrawCard] = useState(false);
-    console.log(drawcard);
+    // const [pets, setPets] = useState(null);
     const getPets = async (token) => {
         const { hostname } = window.location;
         await axios.get(`${apiProtocol}://${hostname}:${apiPort}/pet/info`, {
@@ -24,20 +24,27 @@ const DrawCardsPage = () => {
         })
             .then((res) => {
                 const { data } = res;
-                setPets(data);
+                setHavePets(data);
                 setIsLoading(false);
             });
     };
 
     const startDrawCard = async () => {
-        console.log('start draw card');
-        console.log(drawcard);
         setDrawCard(true);
-        return (
-            <>
-                <h2>start draw card</h2>
-            </>
-        );
+        // const pet = { petName: 'test', petSex: 'Male', petAge: '10' };
+        // setPets(pet);
+        /*
+        const { hostname } = window.location;
+        const token = localStorage.getItem('token');
+        await axios.get(`${apiProtocol}://${hostname}:${apiPort}/pet/draw`, {
+            headers: { token },
+        })
+            .then((res) => {
+                const { data } = res;
+                setPets(data);
+                setIsLoading(false);
+            });
+        */
     };
 
     useEffect(() => {
@@ -53,38 +60,40 @@ const DrawCardsPage = () => {
         </div>
     );
 
-    const NoPet = () => {
-        console.log(pets);
-        return (
-            <>
-                <h4 className="no-pet">{t('drawcards.no-pet')}
-                    <Link to = 'pet/add'>
-                        <Button shape="round" value="small" className="go-add-pet">{t('drawcards.add-pet')}</Button>
-                    </Link>
-                </h4>
-                <div className="draw-card-button">
-                    <Button disabled shape="round" size="large" className="cannot-draw-card" icon={<MehOutlined />}>{t('drawcards.start')}</Button>
-                </div>
-            </>
-        );
-    };
-    const HavePet = () => {
-        console.log(pets);
-        return (
-            <>
-                <h4 className="have-pet">{t('drawcards.have-pet')}
-                    <p>{t('drawcards.description')}</p>
-                </h4>
-                <div className="draw-card-button">
-                    <Button shape="round" size="large" className="start-draw-card" icon={<SmileOutlined /> } onClick={startDrawCard}>{t('drawcards.start')}</Button>
-                </div>
-            </>
-        );
-    };
+    const NoPet = () => (
+        <>
+            <h4 className="no-pet">{t('drawcards.no-pet')}
+                <Link to = 'pet/add'>
+                    <Button shape="round" value="small" className="go-add-pet">{t('drawcards.add-pet')}</Button>
+                </Link>
+            </h4>
+            <div className="draw-card-button">
+                <Button disabled shape="round" size="large" className="cannot-draw-card" icon={<MehOutlined />}>{t('drawcards.start')}</Button>
+            </div>
+        </>
+    );
+    const HavePet = () => (
+        <>
+            <h4 className="have-pet">{t('drawcards.have-pet')}
+                <p>{t('drawcards.description')}</p>
+            </h4>
+            <div className="draw-card-button">
+                <Button shape="round" size="large" className="start-draw-card" icon={<SmileOutlined /> } onClick={startDrawCard}>{t('drawcards.start')}</Button>
+            </div>
+        </>
+    );
+    // const ShowResult = () => {
+    //     console.log(drawcard);
+    //     console.log(pets);
+    //     return (
+    //         <div className="draw-card-result">
+    //             { pets.petName }
+    //         </div>
+    //     );
+    // };
     return (
         <>
-            { !drawcard && <div className="description"><HeadImage/><div className="title"><h3>{t('drawcards.title')}</h3></div> { !isLoading && pets.length !== 0 && <HavePet/> } { !isLoading && pets.length === 0 && <NoPet/> } </div>}
-            { drawcard && <></>}
+            { !drawcard && <div className="description"><HeadImage/><div className="title"><h3>{t('drawcards.title')}</h3></div> { !isLoading && havePets.length !== 0 && <HavePet/> } { !isLoading && havePets.length === 0 && <NoPet/> } </div>}
         </>
     );
 };
