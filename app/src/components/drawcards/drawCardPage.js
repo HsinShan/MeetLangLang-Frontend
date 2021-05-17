3e64bcaa69bec0e3ef5639ab8b14f9f34a246bcc
R09725064 <r09725064@ntu.edu.tw>
2021-05-17 15:55:01 +0800

    feat(app): add condition to start draw card page

--- app/src/components/drawcards/drawCardPage.js
+++ app/src/components/drawcards/drawCardPage.js
@@ -13,9 +13,10 @@ const apiPort = process.env.REACT_APP_API_PORT;
 
 const DrawCardsPage = () => {
     const { t } = useTranslation();
-    // const history = useHistory();
     const [pets, setPets] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [drawcard, setDrawCard] = useState(false);
+    console.log(drawcard);
     const getPets = async (token) => {
         const { hostname } = window.location;
         await axios.get(`${apiProtocol}://${hostname}:${apiPort}/pet/info`, {
@@ -28,44 +29,62 @@ const DrawCardsPage = () => {
             });
     };
 
+    const startDrawCard = async () => {
+        console.log('start draw card');
+        console.log(drawcard);
+        setDrawCard(true);
+        return (
+            <>
+                <h2>start draw card</h2>
+            </>
+        );
+    };
+
     useEffect(() => {
         const token = localStorage.getItem('token');
         getPets(token);
+        setDrawCard(false);
     }, [isLoading]);
+
+    const HeadImage = () => (
+        <div className="img">
+            <img className="cat" src={cat}></img>
+            <img className="dog" src={dog}></img>
+        </div>
+    );
+
     const NoPet = () => {
         console.log(pets);
         return (
-            <h4 className="no-pet">{t('drawcards.no-pet')}
-                <Link to = 'pet/add'>
-                    <Button shape="round" value="small" className="go-add-pet">{t('drawcards.add-pet')}</Button>
-                </Link>
-            </h4>
+            <>
+                <h4 className="no-pet">{t('drawcards.no-pet')}
+                    <Link to = 'pet/add'>
+                        <Button shape="round" value="small" className="go-add-pet">{t('drawcards.add-pet')}</Button>
+                    </Link>
+                </h4>
+                <div className="draw-card-button">
+                    <Button disabled shape="round" size="large" className="cannot-draw-card" icon={<MehOutlined />}>{t('drawcards.start')}</Button>
+                </div>
+            </>
         );
     };
     const HavePet = () => {
         console.log(pets);
         return (
-            <h4 className="have-pet">{t('drawcards.have-pet')}
-                <p>{t('drawcards.description')}</p>
-            </h4>
+            <>
+                <h4 className="have-pet">{t('drawcards.have-pet')}
+                    <p>{t('drawcards.description')}</p>
+                </h4>
+                <div className="draw-card-button">
+                    <Button shape="round" size="large" className="start-draw-card" icon={<SmileOutlined /> } onClick={startDrawCard}>{t('drawcards.start')}</Button>
+                </div>
+            </>
         );
     };
     return (
         <>
-            <div className="description">
-                <div className="img">
-                    <img className="cat" src={cat}></img>
-                    <img className="dog" src={dog}></img>
-                </div>
-                <div className="title">
-                    <h3>{t('drawcards.title')}</h3>
-                </div>
-                { !isLoading && pets.length !== 0 && <HavePet/> }
-                { !isLoading && pets.length === 0 && <NoPet/> }
-            </div>
-            <div className="draw-card-button">
-                { pets ? <Button shape="round" size="large" className="start-draw-card" icon={<SmileOutlined /> }>{t('drawcards.start')}</Button> : <Button disabled shape="round" size="large" className="cannot-draw-card" icon={<MehOutlined />}>{t('drawcards.start')}</Button>}
-            </div>
+            { !drawcard && <div className="description"><HeadImage/><div className="title"><h3>{t('drawcards.title')}</h3></div> { !isLoading && pets.length !== 0 && <HavePet/> } { !isLoading && pets.length === 0 && <NoPet/> } </div>}
+            { drawcard && <></>}
         </>
     );
 };
