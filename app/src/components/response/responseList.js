1ba5ba67d311e094aa3fb79e9083a9bd9b52a0f7
kyec1118 <kyec1118@gmail.com>
2021-05-19 20:56:45 +0800

    feat(app): complete display reply ui

---
+++ app/src/components/response/responseList.js
@@ -0,0 +1,53 @@
+import {
+    Comment,
+    List,
+    Avatar,
+    Divider,
+    Tooltip,
+} from 'antd';
+import { EditOutlined, MessageOutlined } from '@ant-design/icons';
+import React from 'react';
+import moment from 'moment';
+import 'moment/locale/zh-tw';
+import { useTranslation } from 'react-i18next';
+
+const ResponseList = ({ data }) => {
+    const { t } = useTranslation();
+    const sortedData = data.sort((a, b) => b.respId - a.respId);
+    return (
+        <>
+            {(data.length !== 0) &&
+                <List
+                    className="response-list"
+                    icon={<MessageOutlined />}
+                    header={`${t('discuss.response-total')} ${data.length} ${t('discuss.comments')}`}
+                    itemLayout="horizontal"
+                    dataSource={sortedData}
+                    renderItem={(item) => (
+                        <li>
+                            <Comment
+                                datetime={
+                                    <Tooltip title={moment(item.time).format('YYYY-MM-DD HH:mm')}>
+                                        <span>{moment(moment(item.time).local()).fromNow()}</span>
+                                    </Tooltip>}
+                                author={item.author}
+                                content={item.content}
+                                avatar={<Avatar size="large">{item.author.charAt(0)}</Avatar>}
+                            />
+                            <Divider />
+                        </li>
+                    )}
+                />}
+            {(data.length === 0) &&
+                <div className='no-comment'>
+                    <div>
+                        <EditOutlined />
+                        <p>目前還沒有留言喔，幫這篇文章留第一個留言吧！</p>
+                    </div>
+                </div>
+            }
+        </>
+    );
+};
+
+export default ResponseList;
