7d28fc6c85aefd4e7631d889d32062d1ca310421
HsinShan <shirleyhsieh825@gmail.com>
2021-05-24 13:57:31 +0800

    feat(app): adjust drawcards page no pet style and add rwd

--- app/src/components/drawcards/haveNoPet.js
+++ app/src/components/drawcards/haveNoPet.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'antd';
-import { MehOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import '../../assets/style/drawcards/haveNoPet.scss';
 
@@ -9,17 +8,12 @@ const NonePet = () => {
     const { t } = useTranslation();
 
     return (
-        <>
-            <h4 className="no-pet-message">{t('drawcards.no-pet')}
-                <Link to = 'pet/add'>
-                    <Button shape="round" value="small" className="go-add-pet">{t('drawcards.add-pet')}</Button>
-                </Link>
-            </h4>
-            <p></p>
-            <div className="draw-card-button">
-                <Button disabled shape="round" size="large" className="cannot-draw-card" icon={<MehOutlined />}>{t('drawcards.start')}</Button>
-            </div>
-        </>
+        <div className="no-pet-message">
+            <h4>{t('drawcards.no-pet')}</h4>
+            <Link to = 'pet/add'>
+                <Button shape="round" value="small" className="go-add-pet">{t('drawcards.add-pet')}</Button>
+            </Link>
+        </div>
     );
 };
 
