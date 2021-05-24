b017ed193bbe0aaee434f30b1012e68fb91d011c
HsinShan <shirleyhsieh825@gmail.com>
2021-05-24 14:35:24 +0800

    feat(app): adjust add pet page style and modify kind selection key

--- app/src/pages/pet/add.js
+++ app/src/pages/pet/add.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import PetForm from '../../components/pet/petForm';
+import '../../assets/style/pet/index.scss';
 
 const AddPet = () => {
     const { t } = useTranslation();
     return (
-        <div>{t('petadd.pet-name')}</div>,
-        <PetForm />
+        <div className="pet-add">
+            <div className="title">{t('petadd.title')}</div>
+            <PetForm />
+        </div>
     );
 };
 export default AddPet;
